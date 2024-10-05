b41f258d48417a25f8dfa9f720efead2b2a65474
hl <howdefi@gmail.com>
2024-10-04 22:47:15 -0700

    chore: remove some files

--- app/routes/maintenance.tsx
+++ app/routes/maintenance.tsx
@@ -1,7 +1,7 @@
 export default function Screen() {
   return (
     <div
-      className="h-full w-full flex flex-col items-center justify-center min-h-screen w-screen text-white"
+      className="h-full flex flex-col items-center justify-center min-h-screen w-screen text-white"
       style={{
         backgroundImage:
           "radial-gradient( circle farthest-corner at 10% 20%,  rgba(237,3,32,0.87) 20.8%, rgba(242,121,1,0.84) 74.4% )",
